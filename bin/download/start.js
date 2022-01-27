3448a4920934c492aeedcc7bec74236c05beabc6
MrMysterius <official.mrmysterius@gmail.com>
2022-01-28 00:32:37 +0100

    Added download starter

---
+++ bin/download/start.js
@@ -0,0 +1,15 @@
+const { download } = require("./download");
+
+async function startDownload(id, page_count, folder_path) {
+  const base_url = `https://i.nhentai.net/galleries/${id}`;
+
+  const promises = [];
+
+  for (let i = 0; i < page_count; i++) {
+    promises.push(download(`${base_url}/${i + 1}`, folder_path, i + 1, i * 300));
+  }
+
+  await Promise.all(promises);
+}
+
+module.exports.startDownload = startDownload;
